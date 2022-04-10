6ce5aad80cbfe14a1ecfcbb0b70ecb4b04763cff
Rafaa <bouazizrafaa@gmail.com>
2022-04-10 01:50:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def palaindrome(word):
+    inverseword="".join(reversed(word))
+    if inverseword==word:
+         print(word + " is a palndrome")
+    else:
+        print(word + " is not a palndrome")
+word=input("give a non: ")
+
+
+    
+    
